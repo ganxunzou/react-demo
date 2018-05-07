43c4c62d5428ecd524860b310cae5dd6ff6ef05f
GXZ <GXZ@GXZdeiMac.local>
2018-05-07 17:45:37 +0800

    add migrating

--- src/services/userService.js
+++ src/services/userService.js
@@ -1,31 +1,29 @@
-import xhr from './xhr/'
+import xhr from './xhr/';
 
 /**
  * 对应后端涉及到用户认证的 API
  */
 class UserService {
-
-  checkLogin () {
-    return xhr({ url: '/user' })
+  checkLogin() {
+    return xhr({ url: '/user' });
   }
 
   /**
    * @param  {Object} userData
    * @return {Promise}
    */
-  login (userData) {
+  login(userData) {
     return xhr({
-      method: 'post',
-      url: '/login',
-      body: userData
-    })
+      method: 'GET',
+      url: 'api/v2/login',
+      body: userData,
+    });
   }
 
-  logout () {
-    return xhr({ url: '/logout' })
+  logout() {
+    return xhr({ url: '/logout' });
   }
-
 }
 
 // 实例化后再导出
-export default new UserService()
+export default new UserService();
